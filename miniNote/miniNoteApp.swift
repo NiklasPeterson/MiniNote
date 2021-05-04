50b4d4aa9b4d402b0a94fc40e269e130dbbd01d1
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2021-05-04 16:26:44 +0200

    Initial Commit

---
+++ miniNote/miniNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniNoteApp.swift
+//  miniNote
+//
+//  Created by Niklas Peterson on 2021-05-04.
+//
+
+import SwiftUI
+
+@main
+struct miniNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
